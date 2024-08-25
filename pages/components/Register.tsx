c0a220c1bca066ea5ba85622dfe641aadeb19afc
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-08-25 19:51:41 +0100

    Chore: setting headers

--- pages/components/Register.tsx
+++ pages/components/Register.tsx
@@ -13,7 +13,7 @@ const Register = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('/api/auth/Register', { name, email, password });
+            const response = await axios.post('/api/auth/Register', { name, email, password }, { headers: { 'Content-Type': 'application/json' } } );
             localStorage.setItem('token', response.data);
             router.push('/components/Login');
         } catch (error) {
